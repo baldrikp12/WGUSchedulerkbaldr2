7b4bb624596fc4112f773688fa917106a7bd8f1b
Ken B <baldrikp12@gmail.com>
2023-09-23 22:51:15 -0700

    Started Assessment Detail layout TODO: add actionbar to term and course list pages with just title TODO: work on Assessments

--- app/src/main/java/wgu/c192/wguschedulerkbaldr2/entities/Assessment.java
+++ app/src/main/java/wgu/c192/wguschedulerkbaldr2/entities/Assessment.java
@@ -1,39 +1,43 @@
 package wgu.c192.wguschedulerkbaldr2.entities;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
-import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity(
-        tableName = "assessment",
-        foreignKeys = {
-                @ForeignKey(
-                        entity = Course.class,
-                        parentColumns = "courseID",
-                        childColumns = "assessmentID",
-                        onDelete = ForeignKey.CASCADE,
-                        deferred = true)})
+@Entity(tableName = "assessment")
 public class Assessment {
     
     @PrimaryKey(autoGenerate = true)
+    @NonNull
     private int assessmentID;
     private String assessmentTitle;
-    private int courseID_F; //Foreign Key
+    private int courseID_f;
     
-    public Assessment(String assessmentTitle) {
+    // Additional Fields
+    private String dueDate;
+    private String type;
+    private String description;
+    
+    // Fields for assessment type
+    private boolean isObjective; // True for objective, False for performance
+    
+    public Assessment(String assessmentTitle, int courseID_f) {
         this.assessmentTitle = assessmentTitle;
+        this.courseID_f = courseID_f;
     }
     
+    // Default Constructor
     public Assessment() {
-    
+        // Set default values if needed
+        this.courseID_f = 0; // Default value
     }
     
     public int getCourseID_F() {
-        return courseID_F;
+        return courseID_f;
     }
     
     public void setCourseID_F(int courseID_F) {
-        this.courseID_F = courseID_F;
+        this.courseID_f = courseID_F;
     }
     
     public int getAssessmentID() {
@@ -50,5 +54,37 @@ public class Assessment {
     
     public void setAssessmentTitle(String assessmentTitle) {
         this.assessmentTitle = assessmentTitle;
+    }
+    
+    public String getDueDate() {
+        return dueDate;
+    }
+    
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public boolean isObjective() {
+        return isObjective;
+    }
+    
+    public void setObjective(boolean objective) {
+        isObjective = objective;
     }
 }
--- app/src/main/java/wgu/c192/wguschedulerkbaldr2/ui/AssessmentDetail.java
+++ app/src/main/java/wgu/c192/wguschedulerkbaldr2/ui/AssessmentDetail.java
@@ -1,15 +1,66 @@
 package wgu.c192.wguschedulerkbaldr2.ui;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 import wgu.c192.wguschedulerkbaldr2.R;
+import wgu.c192.wguschedulerkbaldr2.entities.Assessment;
 
 public class AssessmentDetail extends AppCompatActivity {
-
+    
+    private static final int MODE_VIEW = 0;
+    private static final int MODE_ADD = 1;
+    private static final int MODE_EDIT = 2;
+    
+    private int mode;
+    private Assessment selectedAssessment;
+    
+    private TextInputLayout assessmentTextLayout;
+    private EditText assessmentTitleEditText;
+    private TextView assessmentstartDate;
+    private TextView assessmentEndDate;
+    private DatePickerDialog dueDatePicker;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment_detail);
-    }}
+        
+        // Initialize UI elements
+        assessmentTextLayout = findViewById(R.id.assessment_text_input_layout);
+        assessmentTitleEditText = findViewById(R.id.assessmentTitleTextview);
+        assessmentstartDate = findViewById(R.id.assessmentStartDateLabel);
+        assessmentEndDate = findViewById(R.id.assessmentEndDateLabel);
+        
+    }
+
+
+    
+    private void enableEditFields() {
+        assessmentTitleEditText.setEnabled(true);
+        assessmentstartDate.setEnabled(true);
+        assessmentEndDate.setEnabled(true);
+    }
+    
+    private void disableEditFields() {
+        assessmentTitleEditText.setEnabled(false);
+        assessmentstartDate.setEnabled(false);
+        assessmentEndDate.setEnabled(false);
+    }
+    
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+    
+    
+
+
+}
